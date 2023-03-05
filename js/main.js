89cd62797de38a7981a6660f9ccf1da3d8f3c278
Vitaliy Semenov <synthex@bk.ru>
2023-03-05 19:48:24 +0600

    feat: main page navbar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const carouselElement = document.querySelector('#carousel');
 const carousel = new bootstrap.Carousel(carouselElement, {
+  ride: false,
   interval: 10000,
   touch: true,
   pause: 'hover',
   keyboard: true,
-})
+});
+
+const modalAskPhoneCall = document.getElementById('modalAskPhoneCall');
+const askForCallbackModal = new bootstrap.Modal(modalAskPhoneCall, {});
+const userPhoneInput = document.getElementById('userPhone');
+
+modalAskPhoneCall.addEventListener('shown.bs.modal', () => {
+  userPhoneInput.focus();
+});
