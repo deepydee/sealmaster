b5b8701cbb301e2394c52d30d4ea0c24c5bfc859
Vitaliy Semenov <synthex@bk.ru>
2023-03-11 21:31:58 +0600

    feat: testimonials from 2gis API

--- js/main.js
+++ js/main.js
@@ -47,27 +47,27 @@ const renderCurrentYear = () => {
 
 const generateWorkingHoursList = () => {
   const WORK_HOURS = [
-    "08:00 - 19:00",
-    "08:00 - 19:00",
-    "08:00 - 19:00",
-    "08:00 - 19:00",
-    "08:00 - 19:00",
-    "08:00 - 16:00",
-    "08:00 - 16:00",
+    "09:00 - 18:00",
+    "09:00 - 18:00",
+    "09:00 - 18:00",
+    "09:00 - 18:00",
+    "09:00 - 18:00",
+    "10:00 - 17:00",
+    "10:00 - 17:00",
   ];
 
   const WEEK_DAYS = [
-    "ВС",
     "ПН",
     "ВТ",
     "СР",
     "ЧТ",
     "ПТ",
     "СБ",
+    "ВС",
   ];
 
   const now = new Date();
-  const currentDay = now.getDay();
+  const currentDay = now.getDay() - 1;
   const whList = document.getElementById('schedule-list');
 
   WORK_HOURS.forEach((el, idx) => {
@@ -89,3 +89,53 @@ const generateWorkingHoursList = () => {
 
 renderCurrentYear();
 generateWorkingHoursList();
+
+const reviewsUrl = 'https://public-api.reviews.2gis.com/2.0/branches/70000001045235526/reviews?limit=12&is_advertiser=false&fields=reviews.is_verified&without_my_first_review=false&rated=true&sort_by=date_edited&key=37c04fe6-a560-4549-b459-02309cf643ad&locale=ru_KZ';
+
+getReviews(reviewsUrl);
+
+async function getReviews(url) {
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const json = await response.json();
+    console.log(json);
+
+    const carousel = document.getElementById('testimonialsList');
+
+    for (let testimonial of json['reviews']) {
+      let lacateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+      let testimonialCreated = new Date(Date.parse(testimonial['date_created']));
+
+      let template = `
+      <div class="carousel-item">
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="card t-card p-sm-4">
+            <span class="icon icon-quote"></span>
+            <div class="card-body">
+              <figure class="text-start" itemprop="review" itemscope itemtype="http://schema.org/Review">
+                <blockquote class="blockquote">
+                  <p class="card-text" itemprop="reviewBody">${testimonial['text']}</p>
+                </blockquote>
+                <figcaption>
+                  <img src="${testimonial['user']['photo_preview_urls']['64x64']}" class="rounded-circle img-responsive img-fluid me-2"> <b itemprop="author">${testimonial['user']['first_name']}</b>
+                  <time datetime="${testimonial['date_created']}" class="testimonial-date text-muted">${testimonialCreated.toLocaleDateString('ru', lacateOptions)}</time>
+                  <meta itemprop="datePublished" content="${testimonial['date_created']}">
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
+
+      carousel.insertAdjacentHTML('beforeend', template);
+    }
+
+
+  } else {
+    console.log('Ошибка HTTP: ' + response.status);
+  }
+}
